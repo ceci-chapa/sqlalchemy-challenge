93e2619495f1bce57c855ce0c1ae28ec66fa0c61
Celenia Chapa <celeniachapa@Celenias-MBP.attlocal.net>
2021-09-02 02:07:03 -0500

    adding to flask

--- ClimateAnalysis/app.py
+++ ClimateAnalysis/app.py
@@ -4,8 +4,24 @@ from sqlalchemy.orm import Session
 from sqlalchemy import create_engine
 import datetime as dt
 from sqlalchemy import func, FLOAT
+import numpy as np
 
+#################################################
+# Database Setup
+engine = create_engine("sqlite:///../Resources/hawaii.sqlite", echo=False)
+Base = automap_base()
+Base.prepare(engine, reflect=True)
+Base.classes.keys()
+M = Base.classes.measurement
+S = Base.classes.station
+#################################################
+
+
+#################################################
+# Flask Setup
 app = Flask(__name__)
+#################################################
+
 
 @app.route("/")
 def welcome():
@@ -21,20 +37,39 @@ def welcome():
     )
 
 @app.route("/api/v1.0/precipitation")
-def get_prices():
-    engine = create_engine("sqlite:///../Resources/hawaii.sqlite", echo=False)
-    Base = automap_base()
-    Base.prepare(engine, reflect=True)
-    Base.classes.keys()
-    M = Base.classes.measurement
-    S = Base.classes.station
+def get_climate():
     session = Session(engine)
     start_date = dt.date(2017, 8, 23) - dt.timedelta(days=365)
     results = session.query(M.date, M.prcp).\
         filter(M.date >= start_date).all()
     session.close()
 
+    # Create a dictionary 
+    all_pre = []
+    for date, prcp in results:
+        date_dict = {}
+        date_dict["date"] = date
+        date_dict["prcp"] = prcp
+        all_pre.append({date : prcp})
 
+    return jsonify(all_pre)
+
+@app.route("/api/v1.0/stations")
+def get_station():
+    session = Session(engine)
+    results = session.query(S.station).distinct().all()
+    session.close()
+
+    #  # Create a dictionary 
+    # all_stations = []
+    # for station in results:
+    #     station_dict = {}
+    #     station_dict["station"] = station
+    #     all_stations.append(station_dict)
+
+    #print(all_stations)
+    all_stations = list(np.ravel(results))
+    return jsonify(all_stations)
 
 
 if __name__ == '__main__':
